eb7f9054158427f6a8bdd47146cb8e69be3bd314
Muxlisa2007 <muxlisa200707@gmail.com>
2023-07-18 22:15:44 +0500

    5-masal

---
+++ f5.py
@@ -0,0 +1,14 @@
+def main(x):
+    if x > 2:
+        print(True)
+    else:
+        print(False)
+main(1)
+
+
+def code(y):
+    if y <= 7:
+        print(True)
+    else:
+        print(False)
+code(9)
