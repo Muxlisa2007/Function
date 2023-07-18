577358a839931496dc90a2506e3fcc334fa89c0d
Muxlisa2007 <muxlisa200707@gmail.com>
2023-07-18 22:16:48 +0500

    6-masala

---
+++ f6.py
@@ -0,0 +1,14 @@
+def main(x):
+    if x > 0:
+        print(True)
+    else:
+        print(False)
+main(1)
+
+
+def code(y):
+    if y <= -10:
+        print(True)
+    else:
+        print(False)
+code(-11)
